a591af6d5dc2347e76a411666fb5370b5a78591b
Scott Cantisani <scantisani@gmail.com>
2020-10-14 09:12:39 +0100

    Split move text into lines at the last space before 17 characters.

--- move.py
+++ move.py
@@ -4,3 +4,17 @@ class Move:
         self.text = text
 
         self.used = False
+
+    def lines(self):
+        lines = []
+        text_left = self.text
+
+        while len(text_left) > 17:
+            next_17_chars = text_left[:17]
+            last_space_index = next_17_chars.rindex(' ')
+
+            lines.append(text_left[:last_space_index + 1])
+            text_left = text_left[last_space_index + 1:]
+
+        lines.append(text_left)
+        return lines
