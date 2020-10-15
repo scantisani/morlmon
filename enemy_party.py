49871637705f787e73622f2ce0da3d299a4e7c7e
Scott Cantisani <scantisani@gmail.com>
2020-10-15 18:27:49 +0100

    Put majority of moves, mons, enemies in.

--- enemy_party.py
+++ enemy_party.py
@@ -7,20 +7,87 @@ from move import Move
 class EnemyParty:
     ENEMIES = [Enemy(name='SYPHILIS',
                      movesets=[[Move("SYPHILIS used ULCERS! GAUNTY CRISPS' DEFENCE fell!"),
-                               Move('SYPHILIS used SWOLLEN LYMPH NODES!', target_damage_fraction=0.5),
-                               Move("SYPHILIS used BLOTCHY RED RASH! It's super effective!", target_damage_fraction=0.5)
-                                ]],
+                                Move("SYPHILIS used SWOLLEN LYMPH NODES!", target_damage_fraction=0.5),
+                                Move("SYPHILIS used BLOTCHY RED RASH! It's super effective!",
+                                     target_damage_fraction=0.5)],
+                               [Move("SYPHILLIS used FLU LIKE SYMPTOMS!", target_damage_fraction=0.2),
+                                Move("SYPHILIS used SOCIAL STIGMA!", target_damage_fraction=0.2),
+                                Move("SYPHILIS used GASLIGHTING!", target_damage_fraction=0.2)]],
                      max_health=100,
                      level=14,
                      sprite=pygame.image.load('images/syphilis.png')),
+
                Enemy(name='THE WHEEL',
-                     movesets=[[Move("The Wheel spins... It lands on JOAN OF ARC! JOAN OF ARC appears and instantly "
+                     movesets=[[],
+
+                               [Move("The Wheel spins... It lands on JOAN OF ARC! JOAN OF ARC appears and instantly "
                                      "dies of SCURVY."),
-                               Move("The Wheel spins... It lands on THICC! Nice. FUNNY HEIR'S HORNINESS rose!"),
-                               Move("The Wheel spins... It lands on INSTANT DEATH!")]],
+                                Move("The Wheel spins... It lands on THICC! Nice. FUNNY HEIR'S HORNINESS rose!"),
+                                Move("The Wheel spins... It lands on INSTANT DEATH!", self_damage_fraction=1)]],
                      max_health=100,
                      level=14,
-                     sprite=pygame.image.load('images/wheel.png'))
+                     sprite=pygame.image.load('images/wheel.png')),
+
+               Enemy(name='BORLEY',
+                     movesets=[[Move("BORLEY casts his gaze over you and readies his SPECIAL ATTACK."),
+                                Move("BORLEY places his hands over the keyboard and prepares to bypass democracy."),
+                                Move("BORLEY begins PLAYING CRUSADER KINGS 2! He's clicking through text boxes faster "
+                                     "than polls can handle! Critical Hit!",
+                                     target_damage_fraction=0.9),
+                                Move("BORLEY got too big for his boots and is being reprimanded by chat. He now "
+                                     "denies everything. BORLEY logs off!", self_damage_fraction=1)]],
+                     max_health=100,
+                     level='30?',
+                     sprite=pygame.image.load('images/borley.png')),
+
+               Enemy(name='FNYHEIR (DRGN)',
+                     movesets=[[],
+
+                               [],
+
+                               [Move("FUNNY HEIR sips from his WIFEHOUSE MUG, available on ETSY.COM/SHOP/MORLMERCH."),
+                                Move("FUNNY HEIR yells at ANDREW ELDRITCH! ANDREW ELDRITCH'S SELF ESTEEM sharply "
+                                     "falls! ANDREW ELDRITCH retires to SHEFFIELD!",
+                                     target_damage_fraction=1)],
+
+                               [Move("Your horrible father, FUNNY HEIR, has frozen you! "
+                                     "You're frozen solid! Ts and Hs in the chat! "
+                                     "FUNNY HEIR became King of Blue Country! FUNNY HEIR'S BOURGEOISIE rose! "
+                                     "You defrosted!"),
+                                Move("FUNNY HEIR feels guilty about freezing the child of destiny. FUNNY HEIR'S SELF "
+                                     "ESTEEM falls!"),
+                                Move("FUNNY HEIR sends MARTIINUS a birthday card to apologise for being a BAD FATHER! "
+                                     "It's not very effective..."),
+                                Move("FUNNY HEIR dies of DEPRESSION!",
+                                     self_damage_fraction=1)]],
+                     max_health=100,
+                     level=71,
+                     sprite=pygame.image.load('images/funnyheir_dragon.png')),
+
+               Enemy(name='TWO GOATS',
+                     movesets=[[],
+
+                               [Move("The TWO GOATS offer you some SHOWER PORRIDGE. You refuse and they begin to get "
+                                     "hysterical."),
+                                Move("The TWO GOATS ask the difference between PORTENCE and IMPORTANCE. You say "
+                                     "'WHAT?!' and they begin to CRY.")],
+
+                               [Move("The TWO GOATS get more hysterical."),
+                                Move("It's not helping."),
+                                Move("The TWO GOATS call it a day and go eat some fudge. The TWO GOATS log off!",
+                                     self_damage_fraction=1)]],
+                     max_health=100,
+                     level=69,
+                     sprite=pygame.image.load('images/twogoats.png')),
+
+               Enemy(name='URIEL',
+                     movesets=[[Move("URIEL makes everyone hate you!", target_damage_fraction=0.25),
+                                Move("URIEL raises a revolt in GRANNY'S GAFF!", target_damage_fraction=0.25),
+                                Move("URIEL gives the AZTECS oars! The AZTECS invade PISSING MYSELF!",
+                                     target_damage_fraction=0.25)]],
+                     max_health=100,
+                     level=943,
+                     sprite=pygame.image.load('images/uriel.png')),
                ]
 
     def __init__(self):
--- party.py
+++ party.py
@@ -20,6 +20,7 @@ class Party:
                 max_health=55,
                 level=17,
                 sprite=pygame.image.load('images/gauntycrisps.png')),
+
             Mon(name='LUFTI',
                 movesets=[[MonMove('SUMMON WITCHES',
                                    "MORGANA, CIRCE, JEZEBEL and their Satanic armies come to your rescue. They put "
@@ -32,10 +33,158 @@ class Party:
                                    "You make FUNNY HEIR your Chancellor, but he doesn't seem to like you..."),
                            MonMove('GREET SPAMBOT',
                                    "A Russian spambot called LEUDUSTNETTICAR85MIK offers to build you a website. You "
-                                   "make him your Court Physician. Why?")]],
+                                   "make him your Court Physician. Why?")],
+
+                          [MonMove('CALL ON SATAN',
+                                   "Before you can do anything, FUNNY HEIR takes you to the woods and kills you.",
+                                   self_damage_fraction=1),
+                           MonMove('OPEN THE ARK',
+                                   "Before you can do anything, FUNNY HEIR takes you to the woods and kills you.",
+                                   self_damage_fraction=1),
+                           MonMove('KILL FUNNY HEIR',
+                                   "Before you can do anything, FUNNY HEIR takes you to the woods and kills you.",
+                                   self_damage_fraction=1),
+                           MonMove('SPAMBOT POPE',
+                                   "Before you can do anything, FUNNY HEIR takes you to the woods and kills you.",
+                                   self_damage_fraction=1)]],
                 max_health=31,
                 level=8,
-                sprite=pygame.image.load('images/lufti.png'))
+                sprite=pygame.image.load('images/lufti.png')),
+
+            Mon(name='FUNNY HEIR',
+                movesets=[[MonMove('BISEXUALITY',
+                                   "You meet a LUSH COUNTRY BEAR and seduce him on the riverbank. FUNNY HEIR's "
+                                   "HORNINESS rose!"),
+                           MonMove('BAN TROUSERS',
+                                   "You remove your trousers. Everyone else joins in. FUNNY HEIR'S HORNINESS rose!"),
+                           MonMove('CRUSADE',
+                                   "Everyone sees a large Irish Bear lug the MEME MACHINE around Galilee and says "
+                                   "'fair enough'. FUNNY HEIR'S SELF ESTEEM rose!"),
+                           MonMove('JESUS FORESKIN',
+                                   "You make a grab for the HOLY PREPUCE and slip it over your finger to become more "
+                                   "Holy. Literally nothing happens.")],
+
+                          [MonMove('GOLDEN ARM',
+                                   "You tear off your REAL BEAR ARM and replace it with a GOLDEN ARM. You hit BORLEY "
+                                   "over the head.",
+                                   target_damage_fraction=0.5),
+                           MonMove('LIGHTHOUSE',
+                                   "You simultaneously commission a pro - cuckoldry HARBOUR and an anti - cuckoldry "
+                                   "DON'T SLEEP WITH MY WIFEHOUSE. TWITCH CHAT'S HORNINESS rose!"),
+                           MonMove('MAXIMUM BEARS',
+                                   "You fuck literally everyone and flood the world with Funny Bears. FUNNY HEIR'S "
+                                   "HORNINESS sharply fell!"),
+                           MonMove('KINGS OF LEON',
+                                   "You shave KINGS OF LEON to look like he's in SYSTEM OF A DOWN. NU METAL's "
+                                   "POPULARITY rose!")],
+
+                          [MonMove('DIE', "THE POPE has had enough of this. "
+                                   "THE POPE caves your head in.", self_damage_fraction=1)]],
+                max_health=127,
+                level=44,
+                sprite=pygame.image.load('images/funnyheir_bear.png')),
+
+            Mon(name='GLTTERHOOF',
+                movesets=[[MonMove('IMMORTALITY', "You become Immortal! You instantly die!", self_damage_fraction=1),
+                           MonMove('BE IMMORTAL', "You become Immortal! You instantly die!", self_damage_fraction=1),
+                           MonMove('NEVER DIE', "You become Immortal! You instantly die!", self_damage_fraction=1),
+                           MonMove('LIVE FOREVER', "You become Immortal! You instantly die!", self_damage_fraction=1)]],
+                max_health=52,
+                level=16,
+                sprite=pygame.image.load('images/glitterhoof.png')),
+
+            Mon(name='ANDRWEDRTCH',
+                movesets=[[MonMove('GOPHER',
+                                   "Your LUSH COUNTRY BEAR is actually a LUSH COUNTRY GOPHER and can therefore "
+                                   "assassinate people. It almost works!",
+                                   target_damage_fraction=0.75),
+                           MonMove('BLUE COUNTRY',
+                                   "You associate France with the colour Blue and are therefore appointed its King! "
+                                   "ANDREW ELDRITCH'S BOURGEOISIE rose!"),
+                           MonMove('DREAM',
+                                   "Last night you have a strange vision of the future. You see two goats in "
+                                   "Barnstaple. You become confused!"),
+                           MonMove('SHEFFIELD',
+                                   "You retire to Sheffield to take up a career as a Scryer Horse.")]],
+                max_health=159,
+                level=56,
+                sprite=pygame.image.load('images/eldritch.png')),
+
+            Mon(name='MARTIINUS',
+                movesets=[[MonMove('SUPER SAIYAN',
+                                   "You tap into your hidden Cornish powers and become the hope of the Universe. "
+                                   "MARTIINUS'S ANIME POWER rose!"),
+                           MonMove('URSINE HERESY',
+                                   "We're not making the Bear a Pope fast enough! The prophet Baloo founds BEAR "
+                                   "CATHOLICISM! MARTIINUS'S ORTHODOXY fell!"),
+                           MonMove('AGE 30 YEARS',
+                                   "You apply some old age makeup and totally fuck up your joints and legs.",
+                                   self_damage_fraction=0.5),
+                           MonMove('FROG',
+                                   "You paint yourself green and jump around saying 'ribbit'. "
+                                   "FUNNY HEIR became confused!")],
+
+                          [MonMove('GPREG',
+                                   "You would rather die than entertain these options.",
+                                   self_damage_fraction=1),
+                           MonMove('FLESH PREG',
+                                   "You would rather die than entertain these options.",
+                                   self_damage_fraction=1),
+                           MonMove('POLLINATE',
+                                   "You would rather die than entertain these options.",
+                                   self_damage_fraction=1),
+                           MonMove('HRPREG',
+                                   "You would rather die than entertain these options.",
+                                   self_damage_fraction=1)]],
+                max_health=167,
+                level=59,
+                sprite=pygame.image.load('images/martiinus.png')),
+
+            Mon(name='BALOO',
+                movesets=[[MonMove('BIG MAD CUP',
+                                   "You drink from the HOLY GRAIL. BALOO'S DEFENSE sharply rose!"),
+                           MonMove('TALESPIN?',
+                                   "You decide you're the BALOO from TALESPIN and not the JUNGLE BOOK. BALOO'S CARTOON "
+                                   "TRIVIA rose!"),
+                           MonMove('REMORSELY',
+                                   "You journey to Spain with DALE COOPER to kill SEAN REMORSELY. Your actions hurt "
+                                   "the TWO GOATS emotionally.",
+                                   target_damage_fraction=0.5),
+                           MonMove('DRAGON BALLS',
+                                   "You collect the DRAGON BALLS to revive FUNNY HEIR again, but he dies straight "
+                                   "away. BALOO'S ANIME POWER fell!")],
+
+                          [MonMove('MORALITY PONG',
+                                   "You play URIEL at MORALITY PONG. It takes ages and the ball disappears.",
+                                   target_damage_fraction=0.25),
+                           MonMove('VOIGT KAMPF',
+                                   "URIEL quizzes you to determine your true intentions. Your crowdsourced responses "
+                                   "blow him away.",
+                                   target_damage_fraction=0.25),
+                           MonMove('BEARLARUS',
+                                   "You rename everything BEARLARUS, including URIEL's INSULIN. URIEL can't find his "
+                                   "INSULIN!",
+                                   target_damage_fraction=0.25),
+                           MonMove('OPEN THE ARK',
+                                   "You prize open KING MORL'S MEME MACHINE to find the FIRST MEME. A tidal wave of "
+                                   "MASS CONVERSIONS flattens URIEL'S morale!",
+                                   target_damage_fraction=0.25)],
+
+                          [MonMove('THE',
+                                   "You pick up the LANCE OF LONGINUS and strike the POPE through his heart!",
+                                   target_damage_fraction=1),
+                           MonMove('LANCE',
+                                   "You pick up the LANCE OF LONGINUS and strike the POPE through his heart!",
+                                   target_damage_fraction=1),
+                           MonMove('OF',
+                                   "You pick up the LANCE OF LONGINUS and strike the POPE through his heart!",
+                                   target_damage_fraction=1),
+                           MonMove('LONGINUS',
+                                   "You pick up the LANCE OF LONGINUS and strike the POPE through his heart!",
+                                   target_damage_fraction=1)]],
+                max_health=178,
+                level=63,
+                sprite=pygame.image.load('images/martiinus.png'))
             ]
 
     def __init__(self):
