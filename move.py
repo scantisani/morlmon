40780eb3f2ac7a59a796768d870a53d41f31c662
Scott Cantisani <scantisani@gmail.com>
2020-10-15 12:47:16 +0100

    Split move text on 17th char if there are no spaces beforehand.

--- move.py
+++ move.py
@@ -12,7 +12,9 @@ class Move:
 
         while len(text_left) > 17:
             next_17_chars = text_left[:17]
-            last_space_index = next_17_chars.rindex(' ')
+            last_space_index = next_17_chars.rfind(' ')
+            if last_space_index == -1:
+                last_space_index = 16
 
             lines.append(text_left[:last_space_index + 1])
             text_left = text_left[last_space_index + 1:]
