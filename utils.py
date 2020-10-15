398575efca2548c066b0823d5d4e8777b875bbe3
Scott Cantisani <scantisani@gmail.com>
2020-10-16 00:17:58 +0100

    Increase line length by a character.

--- utils.py
+++ utils.py
@@ -2,11 +2,11 @@ def split_text(text):
     lines = []
     text_left = text
 
-    while len(text_left) > 17:
-        next_17_chars = text_left[:17]
+    while len(text_left) > 18:
+        next_17_chars = text_left[:18]
         last_space_index = next_17_chars.rfind(' ')
         if last_space_index == -1:
-            last_space_index = 16
+            last_space_index = 17
 
         lines.append(text_left[:last_space_index + 1])
         text_left = text_left[last_space_index + 1:]
