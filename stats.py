fb50c3837a91a1988f5eff490a650eb32d1e62a4
Scott Cantisani <scantisani@gmail.com>
2020-10-16 22:26:56 +0100

    Decrease health bar width slowly on damage taken.

--- stats.py
+++ stats.py
@@ -33,6 +33,9 @@ class Stats:
         self.screen = screen
         self.font = font
 
+        self.mon_health_bar_width = self.HEALTH_BAR_WIDTH
+        self.enemy_health_bar_width = self.HEALTH_BAR_WIDTH
+
     def render_mon_stats(self, mon):
         health = self.font.render(str(mon.health).rjust(3, ' '))[0]
         max_health = self.font.render(str(mon.max_health).rjust(3, ' '))[0]
@@ -46,10 +49,12 @@ class Stats:
         name = self.font.render(mon.name)[0]
         self.screen.blit(name, (self.MON_NAME_X, self.MON_NAME_Y))
 
-        health_bar_width = math.floor(mon.health_fraction() * self.HEALTH_BAR_WIDTH)
-        if health_bar_width > 0:
+        self.mon_health_bar_width = self.__calculate_bar_width__(mon.health_fraction(),
+                                                                 self.mon_health_bar_width)
+
+        if self.mon_health_bar_width > 0:
             pygame.draw.rect(self.screen, (96, 96, 96), (self.MON_HEALTH_BAR_X, self.MON_HEALTH_BAR_Y,
-                                                         health_bar_width, self.HEALTH_BAR_HEIGHT))
+                                                         self.mon_health_bar_width, self.HEALTH_BAR_HEIGHT))
 
     def render_enemy_stats(self, enemy):
         level = self.font.render(str(enemy.level))[0]
@@ -58,7 +63,19 @@ class Stats:
         name = self.font.render(enemy.name)[0]
         self.screen.blit(name, (self.ENEMY_NAME_X, self.ENEMY_NAME_Y))
 
-        health_bar_width = math.floor(enemy.health_fraction() * self.HEALTH_BAR_WIDTH)
-        if health_bar_width > 0:
+        self.enemy_health_bar_width = self.__calculate_bar_width__(enemy.health_fraction(),
+                                                                   self.enemy_health_bar_width)
+
+        if self.enemy_health_bar_width > 0:
             pygame.draw.rect(self.screen, (96, 96, 96), (self.ENEMY_HEALTH_BAR_X, self.ENEMY_HEALTH_BAR_Y,
-                                                         health_bar_width, self.HEALTH_BAR_HEIGHT))
+                                                         self.enemy_health_bar_width, self.HEALTH_BAR_HEIGHT))
+
+    def __calculate_bar_width__(self, health_fraction, current_bar_width):
+        correct_bar_width = math.floor(health_fraction * self.HEALTH_BAR_WIDTH)
+
+        if correct_bar_width < current_bar_width:
+            # slowly decrease if we're not at the right value yet
+            return current_bar_width - 3
+        else:
+            # default to the correct bar width if it's >= than our current bar width
+            return correct_bar_width
