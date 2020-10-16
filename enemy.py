8dc43805e765ecb29809c36b81afffadb858a583
Anthony Gallagher <anthonycgallagher@gmail.com>
2020-10-16 16:28:42 +0100

    Changed enemy entry_message to say THE POPE sent out

--- enemy.py
+++ enemy.py
@@ -7,4 +7,5 @@ class Enemy(Mon):
     def __init__(self, name, movesets, level, sprite, epitaph=None):
         # enemies need slightly smaller sprites
         transformed_sprite = pygame.transform.scale(sprite, (192, 192))
-        super().__init__(name, movesets, 100, level, transformed_sprite, epitaph)
+        entry_message = 'THE POPE sent out ' + name + '!'
+        super().__init__(name, movesets, 100, level, transformed_sprite, epitaph, entry_message)
