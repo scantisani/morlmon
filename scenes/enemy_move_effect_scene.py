94075aad1dff549a39c2ff9f5eaa79a1ef02f74c
Scott Cantisani <scantisani@gmail.com>
2020-10-16 21:35:29 +0100

    Make mons/enemies flash when a move affects them.

--- scenes/enemy_move_effect_scene.py
+++ scenes/enemy_move_effect_scene.py
@@ -2,4 +2,14 @@ from scenes.move_effect_scene import MoveEffectScene
 
 
 class EnemyMoveEffectScene(MoveEffectScene):
-    pass
+    def show_enemy_sprite(self):
+        if not self.hurts_self:
+            return True
+
+        return not self.__in_damage_interval__()
+
+    def show_mon_sprite(self):
+        if self.hurts_self:
+            return True
+
+        return not self.__in_damage_interval__()
--- scenes/move_effect_scene.py
+++ scenes/move_effect_scene.py
@@ -1,6 +1,29 @@
+import pygame
+
 from scenes.effect_scene import EffectScene
 
 
 class MoveEffectScene(EffectScene):
     def __init__(self, screen, font, move):
         super().__init__(screen, font, move.text)
+
+        self.hurts_self = move.self_damage_fraction > 0
+
+        self.scene_start = pygame.time.get_ticks()
+        self.damage_intervals = [(0, 100), (200, 300), (400, 500)]
+
+    def show_enemy_sprite(self):
+        if self.hurts_self:
+            return True
+
+        return not self.__in_damage_interval__()
+
+    def show_mon_sprite(self):
+        if not self.hurts_self:
+            return True
+
+        return not self.__in_damage_interval__()
+
+    def __in_damage_interval__(self):
+        ticks_since_start = pygame.time.get_ticks() - self.scene_start
+        return any(start < ticks_since_start < end for (start, end) in self.damage_intervals)
